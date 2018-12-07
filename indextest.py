685af0300dd10a6d59a6330c8a8773c86155a64c
User <User@cr09347.ad.pu.ru>
2018-12-07 13:58:57 +0300

    index tests 	modified:   indexator.py 	modified:   indextest.py

--- indextest.py
+++ indextest.py
@@ -6,7 +6,7 @@ from indexator import Indexator
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.Indexator = Indexator()
+        self.Indexator = Indexator("MakDatabase")
 
     #unittests for indexize method
     def test_input_type_number(self):
@@ -17,109 +17,83 @@ class Test(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.Indexator.indexize('magsorcsareop.txt')
 
-    def test_result_one_file(self):
+    def test_result_one_file_one_word(self):
         filename = open('test.txt', 'w')
         filename.write('word')
         filename.close()
         self.Indexator.indexize('test.txt')
         flag = False
-        files = os.listdir(path = ".")
+        files = os.listdir(path=".")
         if os.name == 'nt':
             for file in files:
-                if file.startswith('database.'):
+                if file.startswith('MakDatabase.'):
                     flag = True
         else:
             for file in files:
-                if file == 'database':
+                if file == 'MakDatabase':
                     flag = True
         self.assertEqual(flag, True)
         dbdict = dict(database)
         expected = {'word':{'test.txt':[Position(0,4)]}}
         self.assertEqual(dbdict, expected)
 
-    def test_result_another_file(self):
-        filename = open('bloodytest.txt', 'w')
-        filename.write('word')
-        filename.close()
-        self.Indexator.indexize('bloodytest.txt')
-        dbdict = dict(database)
-        expected = {'word':{'test.txt': Position(0,4),
-                            'bloodytest.txt': Position(0,4)}}
-        self.assertEqual(dbdict, expected)
-
-    def test_result_another_word(self):
-        filename = open('goddamntest.txt', 'w')
-        filename.write('arena')
-        filename.close()
-        self.Indexator.indexize('goddamntest.txt')
-        dbdict = dict(database)
-        expected = {'word': {'test.txt': Position(0,4),
-                            'bloodytest.txt': Position(0,4)},
-                    'arena': {'goddamntest.txt': Position(0,5)}}
-        self.assertEqual(dbdict, expected)
-
-    def test_result_many_words(self):
-        filename = open('longlongtest.txt', 'w')
+    def test_result_one_file_many_words(self):
+        filename = open('test.txt', 'w')
         filename.write('BRP arena is easy')
         filename.close()
-        self.Indexator.indexize('longlongtest.txt')
+        self.Indexator.indexize('test.txt')
         dbdict = dict(database)
-        expected = {'word': {'test.txt': Position(0,4),
-                            'bloodytest.txt': Position(0,4)},
-                    'arena': {'goddamntest.txt': Position(0,5),
-                              'longlongtest.txt': Position(4,9)},
-                    'BRP': {'longlongtest.txt': Position(0,3)},
-                    'is': {'longlongtest.txt': Position(10,12)},
-                    'easy': {'longlongtest.txt': Position(13,17)}}
+        expected = {'BRP': {'test.txt': Position(0,3)},
+                    'arena': {'test.txt': Position(4,9)},
+                    'is': {'test.txt': Position(10,12)},
+                    'easy': {'test.txt': Position(13,17)}}
         self.assertEqual(dbdict, expected)
 
-    def test_result_two_files(self):
+    def test_result_two_files_one_word(self):
         filename = open('firstrun.txt', 'w')
         filename.write('easy')
         filename.close()
         self.Indexator.indexize('firstrun.txt')
-        dbdict = dict(database)
-        expected = {'word': {'test.txt': Position(0,4),
-                            'bloodytest.txt': Position(0,4)},
-                    'arena': {'goddamntest.txt': Position(0,5),
-                              'longlongtest.txt': Position(4,9)},
-                    'BRP': {'longlongtest.txt': Position(0,3)},
-                    'is': {'longlongtest.txt': Position(10,12)},
-                    'easy': {'longlongtest.txt': Position(13,17),
-                             'firstrun.txt': Position(0,4)}}
-        self.assertEqual(dbdict, expected)
         filename = open('secondrun.txt', 'w')
-        filename.write('too easy')
+        filename.write('easy')
         filename.close()
         self.Indexator.indexize('secondrun.txt')
         dbdict = dict(database)
-        expected = {'word': {'test.txt': Position(0,4), 
-                            'bloodytest.txt': Position(0,4)}, 
-                    'arena': {'goddamntest.txt': Position(0,5), 
-                              'longlongtest.txt': Position(4,9)}, 
-                    'BRP': {'longlongtest.txt': Position(0,3)}, 
-                    'is': {'longlongtest.txt': Position(10,12)}, 
-                    'easy': {'longlongtest.txt': Position(13,17), 
-                             'firstrun.txt': Position(0,4), 
-                             'secondrun.txt': Position(4,8)},
-                    'too': {'secondrun.txt': Position(0,3)}}
+        expected = {'easy': {'firstrun.txt': Position(0,4), 
+                             'secondrun.txt': Position(0,4)}}
+        self.assertEqual(dbdict, expected)
+
+    def test_result_two_files_many_words(self):
+        filename = open('firstrun.txt', 'w')
+        filename.write('too easy')
+        filename.close()
+        self.Indexator.indexize('firstrun.txt')
+        filename = open('secondrun.txt', 'w')
+        filename.write('not so easy')
+        filename.close()
+        self.Indexator.indexize('secondrun.txt')
+        dbdict = dict(database)
+        expected = {'too': {'firstrun.txt': Position(0,3)}, 
+                    'not': {'secondrun.txt': Position(0,3)}, 
+                    'so': {'secondrun.txt': Position(4,6)}, 
+                    'easy': {'firstrun.txt': Position(4,8), 
+                             'secondrun.txt': Position(7,11)}}
         self.assertEqual(dbdict, expected)
 
     def tearDown(self):
-        files = os.listdir(path = ".")
+        files = os.listdir(path=".")
         for file in files:
             if file == 'test.txt':
-                os.remove(file)
-            if file == 'bloodytest.txt':
-                os.remove(file)
-            if file == 'goddamntest.txt':
-                os.remove(file)
-            if file == 'longlongtest.txt':
                 os.remove(file)
             if file == 'firstrun.txt':
                 os.remove(file)
             if file == 'secondrun.txt':
                 os.remove(file)
+            if file.startswith('MakDatabase.'):
+                os.remove(file)
+            if file == 'MakDatabase':
+                os.remove(file)
+        
 
 if __name__ == '__main__':
     unittest.main()
